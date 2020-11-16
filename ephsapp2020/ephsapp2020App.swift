1c03ec1485a1dbdc022d50cfcd47e3f4cb255112
Connor Holm <connorjholm@gmail.com>
2020-11-16 10:28:04 -0600

    Initial Commit

---
+++ ephsapp2020/ephsapp2020App.swift
@@ -0,0 +1,17 @@
+//
+//  ephsapp2020App.swift
+//  ephsapp2020
+//
+//  Created by 90305163 on 11/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ephsapp2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
