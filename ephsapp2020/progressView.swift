1444e88074a622816b0aa4a8d9ee9dd55892c5a1
Connor Holm <connorjholm@gmail.com>
2020-11-18 10:35:08 -0600

    Changed the UI of progress bar

--- ephsapp2020/progressView.swift
+++ ephsapp2020/progressView.swift
@@ -8,10 +8,10 @@ struct ProgressBar: View {
             ZStack(alignment: .leading) {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
                     .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
+                    .foregroundColor(Color(UIColor.red))
                 
                 Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
+                    .foregroundColor(Color(UIColor.red))
                     .animation(.linear)
             }.cornerRadius(45.0)
         }
@@ -29,24 +29,46 @@ struct progressView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("First Hour")
-                .padding(.horizontal, 10)
             HStack {
+                Text("1st")
+                    .padding(.horizontal,10)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(15)
+                    .shadow(color: Color.red, radius: 2)
                 ProgressBar(value: $progressValue1st).frame(height: 20)
             }.padding(.horizontal, 10)
-            Text("Second Hour")
-                .padding(.horizontal, 10)
             HStack {
+                Text("2nd")
+                    .padding(.horizontal,10)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(15)
+                    .shadow(color: Color.red, radius: 2)
                 ProgressBar(value: $progressValue2nd).frame(height: 20)
             }.padding(.horizontal, 10)
-            Text("Third Hour")
-                .padding(.horizontal, 10)
+            
             HStack {
+                Text("3rd")
+                    .padding(.horizontal,10)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(15)
+                    .shadow(color: Color.red, radius: 2)
                 ProgressBar(value: $progressValue3rd).frame(height: 20)
             }.padding(.horizontal, 10)
-            Text("Fourth Hour")
-                .padding(.horizontal, 10)
+            
             HStack {
+                Text("4th")
+                    .padding(.horizontal,10)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(15)
+                    .shadow(color: Color.red, radius: 2)
                 ProgressBar(value: $progressValue4th).frame(height: 20)
             }.padding(.horizontal, 10)
 
