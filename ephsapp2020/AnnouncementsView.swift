6418ce8b89941387458ce88813f40a49e8011e58
Adrian <adrianthe9000@gmail.com>
2020-11-24 10:44:51 -0600

    Added dummy message

--- ephsapp2020/AnnouncementsView.swift
+++ ephsapp2020/AnnouncementsView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct AnnouncementsView: View {
+    
+    var messages = InboxResponce(responce: Messages(messages: [MessageInfo(id: 6, subject: "A message", recipient_ids: "you", last_updated: 197825, mid: "what's this?", author_id: "Your father", message_status: "read", message: "What's poping home boy")]), unread_count: "4")
+    
     var body: some View {
         VStack {
             Text("Announcemts").font(.title)
             Text(InboxRequest())
+            Text("Sender: \(messages.responce.messages[0].author_id)")
             Spacer()
         }
     }
