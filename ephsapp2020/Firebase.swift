2c4e51026816dd0c03ea61e9698ca427cf6e7b80
connorholm <connorjholm@gmail.com>
2020-12-02 04:22:37 -0600

    Added code to connect to firebase

--- ephsapp2020/Firebase.swift
+++ ephsapp2020/Firebase.swift
@@ -5,7 +5,18 @@
 //  Created by 90305163 on 12/1/20.
 //
 
-import Foundation
+import UIKit
 import Firebase
 
-Firebase
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
