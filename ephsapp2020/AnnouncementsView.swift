beb67612dde6a2e08726fa34dfee30592804ef06
Adrian <adrianthe9000@gmail.com>
2020-12-03 10:38:05 -0600

    Changed to list

--- ephsapp2020/AnnouncementsView.swift
+++ ephsapp2020/AnnouncementsView.swift
@@ -19,20 +19,16 @@ struct AnnouncementsView: View {
             
             Button(action: {
                 refresh.refresh(
-                    consumer_key: defaults.string(forKey: keys.consumer_key)!,
-                    consumer_secret: defaults.string(forKey: keys.consumer_secret)!
+                    consumer_key: defaults.string(forKey: keys.consumer_key) ?? "",
+                    consumer_secret: defaults.string(forKey: keys.consumer_secret) ?? ""
                 )
             }, label: {
                 Text("Refresh")
             })
-            Button(action: {
-                refresh.inbox.message[0].last_updated += 1
-                print("Increased last_updated by 1")
-            }, label: {
-                Text("Increase last_updated by 1")
-            })
-            ForEach(0..<refresh.inbox.message.count){ index in
-                MessageDisplay(message: refresh.inbox.message[index])
+            List {
+                ForEach(0..<refresh.inbox.message.count){ index in
+                    MessageDisplay(message: refresh.inbox.message[index])
+                }
             }
         }
     Spacer()
@@ -41,16 +37,12 @@ struct AnnouncementsView: View {
 
 struct MessageDisplay: View {
     var message: Message
+
     var body: some View {
         VStack {
-            Text("\(message.subject)")
-            Text("Last updated \(message.last_updated)")
-        }.foregroundColor(.white)
-        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        .padding(5)
-        .background(Color.red)
-        .cornerRadius(35)
-        .shadow(radius: 5)
+            Text(message.subject)
+            Text("Last updated \(NSDate(timeIntervalSince1970: TimeInterval(message.last_updated)))")
+        }
     }
 }
 
