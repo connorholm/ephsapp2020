3580c16a4d8ed269ecc64abdf117e5f05918ff13
rachelPark1 <60945786+rachelPark1@users.noreply.github.com>
2020-12-07 10:36:24 -0600

    made the assignmentView more concise

--- ephsapp2020/AssignmentView.swift
+++ ephsapp2020/AssignmentView.swift
@@ -10,128 +10,17 @@ import SwiftUI
 
 struct AssignmentsView: View {
     
-    @State private var isExpanded = false
-    @State private var selectedNum = 1
-   
-    @State private var isExpanded2 = false
-    @State private var selectedNum2 = 1
-    
-    @State private var isExpanded3 = false
-    @State private var selectedNum3 = 1
-    
-    @State private var isExpanded4 = false
-    @State private var selectedNum4 = 1
-    
     var body: some View {
         VStack(alignment: .leading, spacing: 15){
             Text("Assignments")
                 .font(.largeTitle)
            
-            DisclosureGroup("Chinese", isExpanded: $isExpanded){
-                ScrollView{
-                VStack{
-                    ForEach(1...20, id: \.self){ num in
-                    Text("\(num)")
-                        .frame( maxWidth: .infinity)
-                        .font(.title3)
-                        .onTapGesture{
-                            self.selectedNum = num
-                            withAnimation{
-                            self.isExpanded.toggle()
-                            }
-                        }
-                        .padding(.all)
-                    
-                    }
-                }
-                }.frame(height : 150)
-                
-            }.accentColor(.black)
-            .font(.title2)
-            .foregroundColor(.black)
-            .padding(.all)
-            .background(Color.red)
-            .cornerRadius(8)
-            
-            DisclosureGroup("IOS", isExpanded: $isExpanded2){
-                ScrollView{
-                VStack{
-                    ForEach(1...20, id: \.self){ num in
-                    Text("\(num)")
-                        .frame( maxWidth: .infinity)
-                        .font(.title3)
-                        .onTapGesture{
-                            self.selectedNum2 = num
-                            withAnimation{
-                            self.isExpanded2.toggle()
-                            }
-                        }
-                        .padding(.all)
-                    
-                    }
-                }
-                }.frame(height : 150)
-                
-            }.accentColor(.black)
-            .font(.title2)
-            .foregroundColor(.black)
-            .padding(.all)
-            .background(Color.red)
-            .cornerRadius(8)
-            
-            DisclosureGroup("AP Economics", isExpanded: $isExpanded3){
-                ScrollView{
-                VStack{
-                    ForEach(1...20, id: \.self){ num in
-                    Text("\(num)")
-                        .frame( maxWidth: .infinity)
-                        .font(.title3)
-                        .onTapGesture{
-                            self.selectedNum3 = num
-                            withAnimation{
-                            self.isExpanded3.toggle()
-                            }
-                        }
-                        .padding(.all)
-                    
-                    }
-                }
-                }.frame(height : 150)
-                
-            }.accentColor(.black)
-            .font(.title2)
-            .foregroundColor(.black)
-            .padding(.all)
-            .background(Color.red)
-            .cornerRadius(8)
-    
-            DisclosureGroup("Calc 2", isExpanded: $isExpanded4){
-                ScrollView{
-                VStack{
-                    ForEach(1...20, id: \.self){ num in
-                    Text("\(num)")
-                        .frame( maxWidth: .infinity)
-                        .font(.title3)
-                        .onTapGesture{
-                            self.selectedNum4 = num
-                            withAnimation{
-                            self.isExpanded4.toggle()
-                            }
-                        }
-                        .padding(.all)
-                    
-                    }
-                }
-                }.frame(height : 150)
-                
-            }.accentColor(.black)
-            .font(.title2)
-            .foregroundColor(.black)
-            .padding(.all)
-            .background(Color.red)
-            .cornerRadius(8)
-            
-        }.padding(.all)
+            eachClass(className: .constant("Chinese"))
+            eachClass(className: .constant("iOS"))
+            eachClass(className: .constant("AP Economics"))
+            eachClass(className: .constant("AP Calculus BC"))
+        }
+        .padding(.all)
         progressView()
     }
 }
@@ -139,5 +28,41 @@ struct AssignmentsView: View {
 struct AssignmentsView_Previews: PreviewProvider {
     static var previews: some View {
         AssignmentsView()
+    }
+}
+
+struct eachClass: View {
+    
+    @Binding var className: String
+    
+    @State private var isExpanded = false
+    @State private var selectedNum = 1
+    
+    var body: some View {
+        DisclosureGroup(className, isExpanded: $isExpanded){
+            ScrollView{
+                VStack{
+                    ForEach(1...20, id: \.self){ num in
+                        Text("\(num)")
+                            .frame( maxWidth: .infinity)
+                            .font(.title3)
+                            .onTapGesture{
+                                self.selectedNum = num
+                                withAnimation{
+                                    self.isExpanded.toggle()
+                                }
+                            }
+                            .padding(.all)
+                    }
+                }
+            }
+            .frame(height : 150)
+        }
+        .accentColor(.black)
+        .font(.title2)
+        .foregroundColor(.black)
+        .padding(.all)
+        .background(Color.red)
+        .cornerRadius(8)
     }
 }
